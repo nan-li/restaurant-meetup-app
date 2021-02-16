f28f7bdac7c5bb33607b9248578106d81dbfee15
Nan Li <nanftw@gmail.com>
2021-02-16 13:12:07 -0800

    Finish crud functions and implement API routes

--- crud.py
+++ crud.py
@@ -20,8 +20,11 @@ def create_user(username, fname, lname, email, password,
                 image_url, about):
     """Create and return a new user."""
 
+    # When I create a new user, I don't pass password in:
     u = User(username=username, fname=fname, lname=lname, 
                 email=email, image_url=image_url, about=about)
+    
+    # Here I set the password_hash with password
     u.set_password(password)
     
     db.session.add(u)
@@ -44,3 +47,61 @@ def create_meetup(name, date, capacity, attendees_count,
     db.session.commit()
 
     return m
+
+
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+    return User.query.get(user_id)
+
+def get_favorites_by_user_id(user_id):
+    """Return all favorite restaurants of user."""
+    user = get_user_by_id(user_id)
+    return user.favorites
+
+def get_hosted_meetups_by_user_id(user_id):
+    """Return all meetups hosted by user."""
+    user = get_user_by_id(user_id)
+    return user.hosted_meetups
+
+def get_meetups_by_user_id(user_id):
+    """Return all meetups user is attending."""
+    user = get_user_by_id(user_id)
+    return user.meetups
+
+
+
+def get_restaurant_by_id(restaurant_id):
+    """Return a restaurant by primary key."""
+    return Restaurant.query.get(restaurant_id)
+
+def get_fans_by_restaurant_id(restaurant_id):
+    """Return all fans of a restaurant."""
+    restaurant = get_restaurant_by_id(restaurant_id)
+    return restaurant.fans
+
+def get_meetups_by_restaurant_id(restaurant_id):
+    """Return all meetups at a restaurant."""
+    restaurant = get_restaurant_by_id(restaurant_id)
+    return restaurant.meetups
+
+
+
+def get_meetup_by_id(meetup_id):
+    """Return a meetup by primary key."""
+    return Meetup.query.get(meetup_id)
+
+def get_host_by_meetup_id(meetup_id):
+    """Return the host of a meetup."""
+    meetup = get_meetup_by_id(meetup_id)
+    return meetup.host
+
+def get_restaurant_by_meetup_id(meetup_id):
+    """Return the restaurant for a meetup."""
+    meetup = get_meetup_by_id(meetup_id)
+    return meetup.restaurant
+
+def get_attendees_by_meetup_id(meetup_id):
+    """Return the attendees of a meetup."""
+    meetup = get_meetup_by_id(meetup_id)
+    return meetup.attendees
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 """Server for Restaurant Meetup app."""
 
-from flask import Flask, render_template, redirect, flash, request, session, jsonify
-from model import connect_to_db
+from flask import (Flask, render_template, redirect, 
+                    flash, request, session, jsonify)
+from model import connect_to_db, User, Restaurant, Meetup
 import crud
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.secret_key = 'dev'
 def show_homepage():
     """View the homepage."""
 
-    return render_template('homepage.html')
+    return render_template('index.html')
 
 @app.route('/sign-up')
 def show_sign_up_page():
@@ -36,17 +37,101 @@ def register_user():
     
     user = crud.create_user(username, fname, lname, email, password, 
                 image_url, about)
-
     session['user_id'] = user.id
-    session['fname'] = fname
-    session['lname'] = lname
-
-
+    
     flash('Account successfully created.')
 
     return redirect('/')
+
+"""
+API Routes.
+"""
+
+@app.route('/api/users/<int:id>.json')
+def get_user(id):
+    """Return user information."""
+    return jsonify(crud.get_user_by_id(id).to_dict())
+
+
+@app.route('/api/users/<int:user_id>/restaurants.json')
+def get_user_favorites(user_id):
+    """Return a list of the user's favorite restaurants."""
+
+    restaurants = crud.get_favorites_by_user_id(user_id)
+
+    restaurants_info = [r.to_dict() for r in restaurants]
+
+    return jsonify(restaurants_info)
+
+
+@app.route('/api/users/<int:user_id>/hosting.json')
+def get_user_hosted_meetups(user_id):
+    """Return a list of meetups the user is hosting."""
+
+    meetups = crud.get_hosted_meetups_by_user_id(user_id)
+    meetups_info = [m.to_dict() for m in meetups]
+        
+    return jsonify(meetups_info)
+
+
+@app.route('/api/users/<int:user_id>/meetups.json')
+def get_user_meetups(user_id):
+    """Return a list of meetups the user is attending."""
+    
+    meetups = crud.get_meetups_by_user_id(user_id)
+    meetups_info = [m.to_dict() for m in meetups]
+        
+    return jsonify(meetups_info)
+
+
+@app.route('/api/restaurants/<id>.json')
+def get_restaurant(id):
+    """Return restaurant information."""
+    return jsonify(crud.get_restaurant_by_id(id).to_dict())
+
+
+@app.route('/api/restaurants/<restaurant_id>/meetups.json')
+def get_restaurant_meetups(restaurant_id):
+    """Return a list of meetups at this restaurant."""
+    meetups = crud.get_meetups_by_restaurant_id(restaurant_id)
+    meetups_info = [m.to_dict() for m in meetups]
+    return jsonify(meetups_info)
+
+
+@app.route('/api/restaurants/<restaurant_id>/fans.json')
+def get_restaurant_fans(restaurant_id):
+    """Return a list of users favoriting the restaurant."""
+    fans = crud.get_fans_by_restaurant_id(restaurant_id)
+    fans_info = [f.to_dict() for f in fans]
+    return jsonify(fans_info)
+
+
+@app.route('/api/meetups/<int:id>.json')
+def get_meetup(id):
+    """Return meetup information."""
+    return jsonify(crud.get_meetup_by_id(id).to_dict())
+
+
+@app.route('/api/meetups/<int:meetup_id>/host.json')
+def get_host(meetup_id):
+    """Return host information for a meetup."""
+    return jsonify(crud.get_host_by_meetup_id(meetup_id).to_dict())
+
+
+@app.route('/api/meetups/<int:meetup_id>/restaurant.json')
+def get_meetup_restaurant(meetup_id):
+    return jsonify(crud.get_restaurant_by_meetup_id(meetup_id).to_dict())
+
+
+@app.route('/api/meetups/<int:meetup_id>/attendees.json')
+def get_meetup_attendees(meetup_id):
+    """Return a list of attendees for the meetup."""
+    attendees = crud.get_attendees_by_meetup_id(meetup_id)
+    attendees_info = [a.to_dict() for a in attendees]
+    return jsonify(attendees_info)
 
 
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
+
